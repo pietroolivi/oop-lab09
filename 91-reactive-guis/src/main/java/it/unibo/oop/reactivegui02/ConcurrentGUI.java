50d71a2b8a8a94dafbf57d8ee475a1fda9453191
Pietro Olivi <pietroolivi@MacBook-Pro-di-Pietro.local>
2022-11-28 16:12:34 +0100

    Implemented constructor & inner class

--- 91-reactive-guis/src/main/java/it/unibo/oop/reactivegui02/ConcurrentGUI.java
+++ 91-reactive-guis/src/main/java/it/unibo/oop/reactivegui02/ConcurrentGUI.java
@@ -2,6 +2,8 @@ package it.unibo.oop.reactivegui02;
 
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.lang.reflect.InvocationTargetException;
+import java.awt.FlowLayout;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -13,4 +15,69 @@ import javax.swing.SwingUtilities;
  * Second example of reactive GUI.
  */
 public final class ConcurrentGUI extends JFrame {
+
+    private static final long serialVersionUID = 1L;
+    private static final double WIDTH_PERC = 0.2;
+    private static final double HEIGHT_PERC = 0.1;
+    private final JLabel display = new JLabel();
+    private final JButton stop = new JButton("stop");
+    private final JButton up = new JButton("up");
+    private final JButton down = new JButton("down");
+
+    public ConcurrentGUI() {
+
+        super();
+        final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        this.setSize((int) (screenSize.getWidth() * WIDTH_PERC), (int) (screenSize.getHeight() * HEIGHT_PERC));
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        final JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout());
+        panel.add(display);
+        panel.add(up);
+        panel.add(down);
+        panel.add(stop);
+        this.getContentPane().add(panel);
+        this.setVisible(true);
+        
+        final Agent agent = new Agent();
+        new Thread(agent).start();
+        
+        up.addActionListener((e) -> agent.isIncremental = true);
+        down.addActionListener((e) -> agent.isIncremental = false);
+        stop.addActionListener((e) -> {
+            agent.stopCounting();
+            stop.setEnabled(false);
+            up.setEnabled(false);
+            down.setEnabled(false);
+        });
+    }
+
+    private class Agent implements Runnable {
+        
+        private volatile boolean stop;
+        private int counter = 0;
+        private volatile boolean isIncremental = true;
+        
+        @Override
+        public void run() {
+            try {
+                while (!this.stop) {
+                    final var nextInt = Integer.toString(this.counter);
+                    SwingUtilities.invokeAndWait(() -> ConcurrentGUI.this.display.setText(nextInt));
+                    if (this.isIncremental) {
+                        this.counter ++;
+                    } else {
+                        this.counter --;
+                    }
+                    Thread.sleep(100);
+                }
+            } catch(InvocationTargetException | InterruptedException ex) {
+                    ex.printStackTrace();
+            }
+        } 
+
+        public void stopCounting() {
+            this.stop = true;
+        }
+    }
 }
